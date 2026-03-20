356f6cd3fc7061580242ef013d359717935e60a9
Yordan Raynov <YRRaynov24@codingburgas.bg>
2026-03-20 19:11:59 +0200

    Add comments to gui.cpp

--- nexusSchoolSoftware/gui.cpp
+++ nexusSchoolSoftware/gui.cpp
@@ -5,10 +5,12 @@
 
 using namespace std;
 
+// Displays the main menu screen with all primary navigation options
 void showMainMenu()
 {
     clearScreen();
     printAsciiTitle();
+    // List all available menu options
     cout << " 1. Lessons\n";
     cout << " 2. Take IT Test\n";
     cout << " 3. View Statistics\n";
@@ -16,11 +18,12 @@ void showMainMenu()
     printThinLine();
     cout << "Choose option: ";
 }
-
+// Displays the lessons submenu, listing all available lesson modules
 void showLessonsMenu()
 {
     clearScreen();
     printCenteredTitle("LESSON MODULES");
+    // List all available lesson topics
     cout << " 1. HTML Basics\n";
     cout << " 2. CSS Basics\n";
     cout << " 3. JavaScript Basics\n";
@@ -28,7 +31,7 @@ void showLessonsMenu()
     printThinLine();
     cout << "Choose lesson: ";
 }
-
+// Pauses the current screen and waits for the user to type 0 before navigating back
 void waitForEnter()
 {
     cout << "\nType 0 and press Enter to go back: ";
